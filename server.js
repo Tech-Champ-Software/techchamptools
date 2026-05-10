c1f19fac3ed37056b35b5624c76aab9604d2fd1b
Alok sharma <aloksharma7834@gmail.com>
2026-05-10 21:00:54 +0545

    Server.js added

---
+++ server.js
@@ -0,0 +1,24 @@
+const { createServer } = require("http");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "0.0.0.0";
+const port = process.env.PORT || 3000;
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      await handle(req, res);
+    } catch (err) {
+      console.error("Error occurred handling request:", err);
+      res.statusCode = 500;
+      res.end("internal server error");
+    }
+  })
+    .listen(port, hostname, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
